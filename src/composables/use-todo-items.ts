c9dfea92b289da335dc989a32f670914bb732f66
Petr Marek <dev.petrmarek@gmail.com>
2026-02-20 15:07:20 +0100

    feat: fetch todos from api

--- src/composables/use-todo-items.ts
+++ src/composables/use-todo-items.ts
@@ -1,14 +1,30 @@
-import { ref } from 'vue'
-import { todosList } from "../todo-data";
+import { computed, onMounted, ref } from 'vue'
 
 export function useTodoItems() {
-    const todos = ref(todosList);
-    const todosDone = ref(0);
+    const todos = ref([]);
 
-    const handleTodosChanged = (newTodos) => {
-        todosDone.value = newTodos.filter(todo => todo.checked).length;
+    onMounted(async() => {
+        try {
+            const result = await fetch('/api/todos')
+            const items = await result.json()
+            todos.value = items.map((item) => ({
+                id: item.id,
+                label: item.title,
+                checked: item.completed,
+            }))
+        }
+        catch (e) {
+            console.error(e)
+        }
+    })
+
+    const handleTodosChanged = (newTodos) => {        
         todos.value = newTodos;
     };
+
+    const todosDone = computed(() => {
+        return todos.value.filter(todo => todo.checked).length;
+    })
 
     return {
         todos,
