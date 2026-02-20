a966fccf9937d54dbcd90692814909c4ea9ba82c
Petr Marek <dev.petrmarek@gmail.com>
2026-02-20 16:00:07 +0100

    test: checkTodosDone

--- src/tests/test-use-todo-items.spec.ts
+++ src/tests/test-use-todo-items.spec.ts
@@ -1,12 +1,24 @@
 import { useTodoItems } from '../composables/use-todo-items'
 
-test('todosFetched', () => {
+test('addTodo', () => {
   const { todos } = useTodoItems()
   todos.value = [{
     id: '1',
     label: 'label',
-    checked: 'checked',
+    checked: false,
   }]
 
   expect(todos.value.length).toBe(1)
+})
+
+test('checkTodosDone', () => {
+  const { todos, todosDone } = useTodoItems()
+  todos.value = [{
+    id: '1',
+    label: 'label',
+    checked: true,
+  }]
+
+  expect(todosDone.value).toBe(1)
+
 })
