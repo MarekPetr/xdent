5935228b6969b895fea5652ceda65c9e308f0cfd
Petr Marek <dev.petrmarek@gmail.com>
2026-02-21 10:10:00 +0100

    fix: fetch multiple times

--- src/composables/use-todo-items.ts
+++ src/composables/use-todo-items.ts
@@ -1,20 +1,27 @@
 import { computed, onMounted, ref } from 'vue'
 const todos = ref([]);
+const isFetched = ref(false);
 
 export function useTodoItems() {   
     const todosDone = computed(() => {
         return todos.value.filter(todo => todo.checked).length;
     })
     
-    onMounted(async() => {
+    onMounted(() => {
+        if (isFetched.value) {
+            return
+        }
+        
         try {
-            const result = await fetch('/api/todos')
-            const items = await result.json()
-            todos.value = items.map((item) => ({
-                id: item.id,
-                label: item.title,
-                checked: item.completed,
-            }))
+            fetch('/api/todos')
+                .then(res => res.json())
+                .then(items => todos.value = items.map((item) => ({
+                    id: item.id,
+                    label: item.title,
+                    checked: item.completed,
+                })))
+            
+            isFetched.value = true;
         }
         catch (e) {
             console.error(e)
