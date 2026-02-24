b5c0e6112a401a1576fac9f9d6e9ef2131ffcc35
Petr Marek <dev.petrmarek@gmail.com>
2026-02-24 18:21:35 +0100

    refactor: use await

--- src/composables/use-todo-items.ts
+++ src/composables/use-todo-items.ts
@@ -1,30 +1,32 @@
 import { computed, onMounted, ref } from 'vue'
 const todos = ref([]);
-const isFetched = ref(false);
+let fetchPromise = null;
 
 export function useTodoItems() {   
     const todosDone = computed(() => {
         return todos.value.filter(todo => todo.checked).length;
     })
+
+    const fetchTodos = async () => {
+        try {
+            const res = await fetch('/api/todos')
+            const items = await res.json()
+            todos.value = items.map(item => ({
+                id: item.id,
+                label: item.title,
+                checked: item.completed,
+            }))
+        }
+        catch(e) {
+            fetchPromise = null;
+            console.error(e)
+        }        
+    }
+
     
     onMounted(() => {
-        if (isFetched.value) {
-            return
-        }
-        
-        try {
-            fetch('/api/todos')
-                .then(res => res.json())
-                .then(items => todos.value = items.map((item) => ({
-                    id: item.id,
-                    label: item.title,
-                    checked: item.completed,
-                })))
-            
-            isFetched.value = true;
-        }
-        catch (e) {
-            console.error(e)
+        if (!fetchPromise) {
+            fetchPromise = fetchTodos()
         }
     })
 
